7b5ffabbc40827163a0d2b6dd8381fa88bcc4e93
harshrathore24 <rathoreharsh304@gmail.com>
2024-08-29 10:59:57 +0530

    update ec2

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = var.region   
 }
 
-# resource "aws_instance" "my_instance" {
-#   ami           = "ami-05c3dc660cb6907f0"  
-#   instance_type = "t2.micro"  
+resource "aws_instance" "my_instance" {
+  ami           = "ami-05c3dc660cb6907f0"  
+  instance_type = "t2.micro"  
 
-#   tags = {
-#     Name = "MyInstance"
-#   }
-# }
+  tags = {
+    Name = "MyInstance"
+  }
+}
 
 
 
